e24fc41df0fd3f82c9a30a6b6d54fcebd7f334dd
Avinash <avinash.v@idp.com>
2025-06-03 11:49:20 +0530

    enumerate correction

--- Arrays & Hashing/Two Sum/brute_force.py
+++ Arrays & Hashing/Two Sum/brute_force.py
@@ -3,6 +3,6 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i, num_1 in enumerate(nums):
-            for j, num_2 in enumerate(nums, start=i+1):
-                if num_1 + num_2 == target:
+            for j in range(i+1, len(nums)):
+                if num_1 + nums[j] == target:
                     return [i, j]
